894e0f41d2976e323a07e83d7df121e35a788d7a
Charles Iliya Krempeaux <iamreiver@gmail.com>
2025-08-07 22:00:00 -0700

    named return string to make it more obvious what func is doing

--- resolve.go
+++ resolve.go
@@ -3,7 +3,7 @@ package pymtptr
 // Resolve resolves a Payment-Pointer to an HTTP URL.
 //
 // If the Payment-Pointer is NOT a valid the Resolve returns an empty string.
-func Resolve(paymentPointer string) string {
+func Resolve(paymentPointer string) (httpsURL string) {
 	var pp PaymentPointer
 	err := pp.Parse(paymentPointer)
 	if nil != err {
